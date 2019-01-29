10c159722a909f56eca39625a72208c3fdbaeebf
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 13:25:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,10 +38,8 @@ def hit?(current_total)
     input = get_user_input
     if input == "h"
       new_total += deal_card
-      display_card_total(new_total)
       break
     elsif input == "s"
-      display_card_total(new_total)
       break
     else
       invalid_command
